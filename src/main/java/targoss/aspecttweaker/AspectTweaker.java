0d81803bb1e2610507e31130233742e6f61f65cd
asanetargoss <asanetargoss@users.noreply.github.com>
2018-01-31 18:27:09 -0800

    Initial commit

---
+++ src/main/java/targoss/aspecttweaker/AspectTweaker.java
@@ -0,0 +1,10 @@
+package targoss.aspecttweaker;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = AspectTweaker.MODID, version = AspectTweaker.VERSION)
+public class AspectTweaker
+{
+    public static final String MODID = "aspecttweaker";
+    public static final String VERSION = "0.1";
+}
