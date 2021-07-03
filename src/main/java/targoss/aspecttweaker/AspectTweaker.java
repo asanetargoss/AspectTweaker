3248ad46fb0e79a60b06747d640d035d3f130885
asanetargoss <asanetargoss@users.noreply.github.com>
2021-07-03 14:48:14 -0700

    Version 0.2.0

--- src/main/java/targoss/aspecttweaker/AspectTweaker.java
+++ src/main/java/targoss/aspecttweaker/AspectTweaker.java
@@ -58,7 +58,7 @@ import thaumcraft.api.internal.CommonInternals;
 public class AspectTweaker
 {
     public static final String MODID = "aspecttweaker";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.2.0";
     public static final String DEPENDENCIES = 
     		"required-after:MineTweaker3;" +
     		"required-after:thaumcraft;";
